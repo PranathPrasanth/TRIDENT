f52e753757925e45243fb2f4cbe3a29b8d28f2fc
Pranath Prasanth <pranathp05@gmail.com>
2026-09-13 18:34:51 +0530

    updated api.py

--- src/dashboard/api.py
+++ src/dashboard/api.py
@@ -44,6 +44,7 @@ app.add_middleware(
         "http://127.0.0.1:5173",
         "http://localhost:3000",
         "http://127.0.0.1:3000",
+        "http://192.168.31.222:8080",
     ],
     allow_credentials=True,
     allow_methods=["*"],
